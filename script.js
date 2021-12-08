40361323450d0cf8ef443d0f6cc0e35b26eb3bba
Anderson <andeersonporteela@gmail.com>
2021-12-08 16:34:07 -0300

    few logical implements

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+//                  --- API & KEY ---
 const apikey = "c32ffe255cdfad999d67dfb511e5323e";
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
-const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
-async function getWeatherByLocation(location) {
-    const resp = await fetch(url(location), {
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), {
     origin: "cors"});
-
     const respData = await resp.json();
 
-    console.log(respData, KtoC(respData.main.temp));
-
+    addWeatherToPage(respData);
 }
 
-getWeatherByLocation("Sobral");
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement('div');
+    weather.classList.add('weather');
+
+    weather.innerHTML = `
+        <small>There are</small>
+        <h2>${temp}°C</h2>
+        <p>in ${search.value}</p>`;
+
+
+//          --- cleaning up ---
+
+    main.innerHTML = "";
+
+    
+    main.appendChild(weather);
+}
 
 function KtoC(K) {
     return (K - 273.15).toFixed(2);
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if(city) {
+        getWeatherByLocation(city);
+    }
+});
